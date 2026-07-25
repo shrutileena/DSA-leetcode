1d9eced91f0990f8cceeba5e1fb7d29e4d822440
Shruti Agarwal <shrutileena@gmail.com>
2026-07-25 16:09:25 +0530

    Time: 3 ms (59.03%), Space: 44.4 MB (65.04%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,49 +1,20 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // s=s.replace(" ", "");
-		// s=s.toLowerCase();
-        // int len = s.length();
-        // int left = 0;
-        // int right = len - 1;
-        // boolean isPalindrome = true;
-
-        // while(left<right){
-        //     if(!Character.isLetterOrDigit(s.charAt(left))){
-        //         left++;
-        //     }
-        //     if(!Character.isLetterOrDigit(s.charAt(right))){
-        //         right--;
-        //     }
-        //     if(left>right)
-        //     	break;
-        //     if(s.charAt(left) != s.charAt(right)){
-        //         isPalindrome = false;
-        //     }
-        //     left++;
-        //     right--;
-        // }
-
-        // return isPalindrome;
 
         int left = 0, right = s.length() - 1;
 
-        // Loop to check if the string is a palindrome
         while (left < right) {
-            // Skip non-alphanumeric characters on the left side
             if (!Character.isLetterOrDigit(s.charAt(left))) 
                 left++;
-            // Skip non-alphanumeric characters on the right side
             else if (!Character.isLetterOrDigit(s.charAt(right))) 
                 right--;
-            // If characters are different, it's not a palindrome
             else if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) 
                 return false;
             else {
-                // Move towards the middle if characters are the same
                 left++; 
                 right--;
             }
         }
-        return true;  // The string is a palindrome if the loop completes
+        return true;  
     }
 }
